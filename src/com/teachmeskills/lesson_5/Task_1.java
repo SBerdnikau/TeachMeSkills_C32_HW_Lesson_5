9325d88798f0ee54b321eff07ae266b80f005ed8
Sergey Berdnikov <berdnikausiarhei@gmail.com>
2024-10-13 14:47:26 +0300

    TMC-C32-Task-01
    The task one was completed.

---
+++ src/com/teachmeskills/lesson_5/Task_1.java
@@ -0,0 +1,41 @@
+package com.teachmeskills.lesson_5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 0. Создать трехмерный массив из целых чисел.
+ * С помощью циклов "пройти" по всему массиву и увеличить каждый
+ * элемент на заданное число. Пусть число, на которое будет
+ * увеличиваться каждый элемент, задается из консоли.
+ */
+public class Task_1 {
+    public static void main(String[] args) {
+
+        int[][][] treeDimArray = new int[5][5][5];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert is a magnification factor:");
+        int magnificationFactor = scanner.nextInt();
+
+        for (int i = 0; i < treeDimArray.length; i++){
+            for (int j = 0; j < treeDimArray.length; j++){
+                for (int k = 0; k < treeDimArray.length; k++) {
+                    treeDimArray[i][j][k] = (int)(Math.random() * 100);
+                }
+            }
+        }
+
+        System.out.println(Arrays.deepToString(treeDimArray));
+
+        for (int i = 0; i < treeDimArray.length; i++){
+            for (int j = 0; j < treeDimArray.length; j++){
+                for (int k = 0; k < treeDimArray.length; k++) {
+                    treeDimArray[i][j][k] += magnificationFactor;
+                }
+            }
+        }
+
+        System.out.println(Arrays.deepToString(treeDimArray));
+    }
+}
